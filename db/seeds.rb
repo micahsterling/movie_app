911eb03ee7534c50c3e5459f52d3a850817859e3
Micah Sterling <msterling89@gmail.com>
2021-03-01 17:20:54 -0600

    add actor data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+# actor = Actor.new(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
+# actor.save
+actor = Actor.new(first_name: FFaker::Name.first_name , last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+actor.save
+actor = Actor.new(first_name: FFaker::Name.first_name , last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+actor.save
+actor = Actor.new(first_name: FFaker::Name.first_name , last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+actor.save
+actor = Actor.new(first_name: FFaker::Name.first_name , last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+actor.save
+actor = Actor.new(first_name: FFaker::Name.first_name , last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+actor.save
+actor = Actor.new(first_name: FFaker::Name.first_name , last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+actor.save
+actor = Actor.new(first_name: FFaker::Name.first_name , last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+actor.save
+actor = Actor.new(first_name: FFaker::Name.first_name , last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+actor.save
